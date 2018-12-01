d2428146f00ee2e95a954ac5dfdc82a1ba05b560
Matthew Weems <matthewweems@Matthews-MacBook-Pro.local>
2018-12-01 16:28:54 -0600

    get root of xml file, allow user to start over

--- parser/fileParser.py
+++ parser/fileParser.py
@@ -1,6 +1,7 @@
+import xml.etree.ElementTree as ET
+
+
 def ParseFile(file):
-	data = {
-		'fileName':file,
-		'someData':'something to check'
-	}
-	return data
+	tree = ET.parse(file)
+	root = tree.getroot()
+	return root
--- parser/routes.py
+++ parser/routes.py
@@ -1,17 +1,13 @@
 from parser import app
-from flask import render_template, redirect, url_for, request
+from flask import render_template, url_for, request
 from parser.forms import FileForm
 from fileParser import ParseFile
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	form = FileForm()
 	if form.validate_on_submit():
-		file = request.form['file']
-		data = ParseFile(file)
-		return redirect(url_for('data_view', data=data))
+		data = ParseFile(request.files['file'])
+		return render_template('data.html', data=data)
 	return render_template('index.html', form=form)
-
-@app.route('/data_view/<data>')
-def data_view(data):
-	return render_template('data.html', data=data)
